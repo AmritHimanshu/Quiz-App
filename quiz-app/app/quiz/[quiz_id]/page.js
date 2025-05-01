e74bebfee84c5f46d8b0e7397553e713afc1aca7
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-05-01 23:21:49 +0530

    Working on quiz app

--- quiz-app/app/quiz/[quiz_id]/page.js
+++ quiz-app/app/quiz/[quiz_id]/page.js
@@ -13,21 +13,20 @@ export default function Page() {
   const quizId = params.quiz_id;
   const quizData = quizMap[quizId];
 
-  console.log("quizId:", quizId);
-  console.log("quizData:", quizData);
-
   return (
     <div>
       <Header />
-      <div className='w-[500px] m-auto flex flex-col items-center py-3 space-y-5'>
-        <div className='relative w-[450px] h-[400px]'>
-          <Image src={quizData.image} alt="img" layout="fill" objectFit="cover" />
+      <div className='flex items-center justify-center h-[calc(100vh-90px)]'>
+        <div className='w-[500px] m-auto flex flex-col items-center py-3 space-y-5'>
+          <div className='relative w-[450px] h-[400px] rounded-md overflow-hidden drop-shadow-[0_0_15px_rgba(0,255,255,0.3)]'>
+            <Image src={quizData.image} alt="img" layout="fill" objectFit="cover" />
+          </div>
+          <div className='text-2xl font-bold'>{quizData.name}</div>
+          <div className='text-lg font-semibold'>Answer these simple questions correctly and earn coins</div>
+          <div className='font-semibold'>Difficulty Level: <span className='text-green-100 bg-green-600 p-2 rounded-md'>{quizData.difficulty_level}</span></div>
+          <div>{quizData.noOfQuestions} Questions</div>
+          <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.3)] duration-300'>Play</button>
         </div>
-        <div className='text-2xl font-bold'>{quizData.name}</div>
-        <div className='text-lg font-semibold'>Answer these simple questions correctly and earn coins</div>
-        <div className='font-semibold'>Difficulty Level: <span className='text-green-100 bg-green-600 p-2 rounded-md'>{quizData.difficulty_level}</span></div>
-        <div>{quizData.noOfQuestions} Questions</div>
-        <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer'>Play</button>
       </div>
     </div>
   );
