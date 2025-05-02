9df05cddb9470251c927abd16fa48c8c10baf82f
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-05-02 15:06:16 +0530

    Working on quiz app

--- quiz-app/app/quiz/match/[quiz_id]/page.js
+++ quiz-app/app/quiz/match/[quiz_id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import quizMap from "@/database/quizQuestion";
 import Image from 'next/image';
@@ -11,7 +11,16 @@ function Page() {
   const quizId = params.quiz_id;
   const quizData = quizMap[quizId];
 
-  console.log(quizData);
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  const [timeLeft, setTimeLeft] = useState(30);
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  console.log("Questions: ", questions);
+
+  useEffect(()=>{
+    setQuestions(quizData.questions);
+  },[quizData]);
 
   return (
     <div className='p-3 space-y-10'>
@@ -27,12 +36,11 @@ function Page() {
           <div className='relative w-[250px] h-[200px] rounded-md overflow-hidden'>
             <Image src={quizData.questions[0].image} alt="img" layout="fill" objectFit="contain" />
           </div>
-          <div className='w-full'>Q. {quizData.questions[0].question}</div>
+          <div className='w-full'>Q. {questions[currentQuestionIndex].question}</div>
           <div className='w-full space-y-5'>
-            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>1</span><span>{quizData.questions[0].options[0].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>2</span><span>{quizData.questions[0].options[1].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>3</span><span>{quizData.questions[0].options[2].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>4</span><span>{quizData.questions[0].options[3].option}</span></div>
+            {questions[currentQuestionIndex].options.map((option, idx)=>(
+              <div key={option.id} className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>{idx+1}</span><span>{option.option}</span></div>
+            ))}
           </div>
           <div className='w-full'>
             <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer animate-pulse hover:animate-none hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.3)] duration-300'>Next Question</button>
