d5a51b7b0f5ee75d302bc0f2e5953c2e71134c07
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-05-02 13:20:32 +0530

    Working on quiz app

--- quiz-app/app/quiz/match/[quiz_id]/page.js
+++ quiz-app/app/quiz/match/[quiz_id]/page.js
@@ -29,10 +29,10 @@ function Page() {
           </div>
           <div className='w-full'>Q. {quizData.questions[0].question}</div>
           <div className='w-full space-y-5'>
-            <div className='bg-white text-black p-2 rounded-md space-x-2'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>1</span><span>{quizData.questions[0].options[0].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>2</span><span>{quizData.questions[0].options[1].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>3</span><span>{quizData.questions[0].options[2].option}</span></div>
-            <div className='bg-white text-black p-2 rounded-md space-x-2'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>4</span><span>{quizData.questions[0].options[3].option}</span></div>
+            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>1</span><span>{quizData.questions[0].options[0].option}</span></div>
+            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>2</span><span>{quizData.questions[0].options[1].option}</span></div>
+            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>3</span><span>{quizData.questions[0].options[2].option}</span></div>
+            <div className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300'><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>4</span><span>{quizData.questions[0].options[3].option}</span></div>
           </div>
           <div className='w-full'>
             <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer animate-pulse hover:animate-none hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.3)] duration-300'>Next Question</button>
