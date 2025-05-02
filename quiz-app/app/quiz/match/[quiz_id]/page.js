edba169b8fc0f79a4bcb0d77bfb1f39dce838eb3
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-05-02 17:46:42 +0530

    Working on quiz app

--- quiz-app/app/quiz/match/[quiz_id]/page.js
+++ quiz-app/app/quiz/match/[quiz_id]/page.js
@@ -48,8 +48,12 @@ function Page() {
               <div key={option.id} className='bg-white text-black p-2 rounded-md space-x-2 cursor-pointer hover:bg-gray-400 duration-300' onClick={() => handleOptionSelect(option.option)}><span className='py-1 px-2 rounded-full bg-[#1A1A1A] text-white text-xs'>{idx + 1}</span><span>{option.option}</span></div>
             ))}
           </div>
-          <div className='w-full'>
-            <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer animate-pulse hover:animate-none hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.3)] duration-300'>Next Question</button>
+          <div className='mb-5'>
+            {userAnswers[currentQuestionIndex] && (
+              <div className='w-full'>
+                <button className='p-2 bg-[#39FF14] text-black font-bold w-full rounded-full cursor-pointer animate-pulse hover:animate-none hover:drop-shadow-[0_0_15px_rgba(0,255,255,0.3)] duration-300'>Next Question</button>
+              </div>
+            )}
           </div>
         </div>
       </div>
