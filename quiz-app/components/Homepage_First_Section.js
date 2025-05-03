3f59ab6fb3ec5126806e128a1d71708ce8ccd240
AmritHimanshu <himanshukumaramrit41855@gmail.com>
2025-05-03 11:31:39 +0530

    Working on quiz app

--- quiz-app/components/Homepage_First_Section.js
+++ quiz-app/components/Homepage_First_Section.js
@@ -15,15 +15,13 @@ function Homepage_FIrst_Section() {
       <div className="p-3 md:p-5 absolute top-0 w-full h-full flex items-center justify-center text-white bg-neutral-900/40">
         <div className="flex flex-col lg:flex-row items-start lg:items-center justify-between space-y-10 lg:space-y-0 w-full lg:w-[60%]">
           <div className="w-full lg:w-[50%]">
-            <div className="w-full text-[30px] md:text-[50px] xl:text-[60px] 2xl:text-[80px] font-bold">
-              Pure <span className="text-customGreen">Organic</span> Products
+            <div className="w-full text-[30px] md:text-[50px] xl:text-[60px] 2xl:text-[80px] font-bold leading-tight">
+              Enter the <span className="text-customGreen">QuizVerse</span>
             </div>
           </div>
 
           <div className="w-full lg:w-[45%] text-base md:text-lg lg:text-base 2xl:text-xl">
-            we are dedicated to deliver fresh and natural mangoes which are
-            grown chemical residue free to your door step. We provide all
-            variety of mangoes to our customers.
+            Battle your friends. Earn coins. Climb the ranks. Explore quizzes that test your brain and reward your hustle — all in real-time.
           </div>
         </div>
       </div>
